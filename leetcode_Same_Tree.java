0bbc9d098e81dfe2544f1c380bb8516066cd8122
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-05 11:05:53 +0530

    Update leetcode_Same_Tree.java

--- leetcode_Same_Tree.java
+++ leetcode_Same_Tree.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode p, TreeNode q) {//T(C(N))==O(1) and S(C(N))==O(1) as it requires Constant Time in A constant space
         if(p==null || q==null )return p==q;//checking if TreeNode's existance
         return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right); // Printing  Both the Tree's With Same Nodes
     }
